808b3be77c6faf9168178f0086dcec3b9091b1ea
nlarsen7 <nlarsen2013@gmail.com>
2020-04-07 19:41:58 +0000

    Menu 3.2.2

--- main.go
+++ main.go
@@ -1,3 +1,6 @@
+//Nicholas Larsen
+//4/7/2020
+//a simple menu of food items
 package main
 
 import "fmt"
@@ -10,6 +13,7 @@ func menu() {
   fmt.Println("salad")
   fmt.Println("chicken")
   fmt.Println("pizza")
+  //the menu
   
 }
 func main() {
@@ -18,4 +22,5 @@ var user string
 fmt.Println("choose what you want from the menu")
 fmt.Scanln(&user)
 fmt.Println("you have chosen",user,"as your meal")
+//prints out the menu and has the user choose frim the items
 }
